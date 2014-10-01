baddf327f489742dfc1f6039434f784a2f7304d2
Mikko Pohja <mikko.pohja@futurice.com>
2014-10-01 15:18:55 +0300

    Added a simple graph for verifying samples, average and tolerance

--- audio.js
+++ audio.js
@@ -16,6 +16,7 @@ stash.colorPhase = 0;
 
 stash.peakTolerance = 1.3;
 stash.toleranceUpdated = 0;
+stash.sampleCount = 0;
 
 var initBeatDetection = function() {
 	stash.beatAverage = 0;
@@ -33,6 +34,7 @@ var initBeatDetection = function() {
 	stash.beatBufferLength = 16;
 	stash.beatAverage = 0;
 	stash.beatFound = false;
+
 }
 
 initBeatDetection();
@@ -51,9 +53,29 @@ navigator.getUserMedia = (navigator.getUserMedia ||
                           navigator.msGetUserMedia);
 navigator.getUserMedia( {video: false, audio:true}, userMediaGot, function(e) { console.log(e); } );
 
+var d3Stuff = {};
+
 var audioInit = function(audioFile) {
 	stash.width = $('.d3-visualization').width();
 	stash.height = $('.d3-visualization').height();
+
+	d3Stuff = {
+		className : 'd3-visualization',
+		x : d3.scale.linear().domain([0, stash.peakBufferLength]).range([0, stash.width]),
+		y : d3.scale.linear().domain([0, (stash.barCount / 3) * Math.pow(255,2)]).range([stash.height, 0]),
+		line : d3.svg.line()
+				// assign the X function to plot our line as we wish
+				.x(function(d,i) { 
+					console.log("calculating x for", d, i, d3Stuff.x(i));
+					return d3Stuff.x(i); 
+				}.bind(this))
+				.y(function(d) { 
+					console.log("calculating y for", d, d3Stuff.y(d));
+					return d3Stuff.y(d); 
+				}.bind(this))
+				.interpolate("linear")
+	};
+
 
 	$('.hide-on-play').remove();
 
@@ -169,31 +191,7 @@ var createSoundSource = function(context, audioData) {
 }
 
 var onBeat = function() {
-	/*
-	var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-
-	d3.select('body')
-		.attr('style', 'background-color: ' + color);
-	*/
-
-	var video = document.getElementById('video');
-	//video.pause();
-	video.currentTime = 0;
-	//video.play();
-
-	if( stash.beatCount !== 0 && stash.beatCount % 16 == 0 ) {
-		video.playbackRate = (Math.random() * 5).toFixed(1);
-	}
-
-	d3.selectAll('body')
-		.attr('style', '-webkit-filter: blur(3px);')
-		.transition()
-		.duration(100)
-		.attr('style', '-webkit-filter: blur(0px);');
+	console.log("Beat");
 }
 
 var onBeatFound = function() {
@@ -201,8 +199,13 @@ var onBeatFound = function() {
 	video.playbackRate = 3;
 }
 
+
+
+
 var visualize = function() {
+	
 	var now = new Date().getTime();
+	++stash.sampleCount;
 	stash.analyser.getByteTimeDomainData(stash.dataArray);
 	//requestAnimationFrame(visualize);
 
@@ -217,8 +220,7 @@ var visualize = function() {
 // stash.peakBufferLength = 200; // Around 1s with shortest possible setInterval
 // stash.currentPeakBufferIndex = 0;
 
-	var soundImpulse = _.reduce(stash.dataArray, function(memo, v) { return memo + Math.pow(v, 3); }, 0);
-
+	var soundImpulse = _.reduce(stash.dataArray, function(memo, v) { return memo + Math.pow(v, 2); }, 0);
 	var sum = _.reduce(
 		stash.peakBuffer, 
 		function(memo, v) {  return memo + v; },
@@ -301,8 +303,73 @@ var visualize = function() {
 			: stash.currentPeakBufferIndex + 1;
 	}
 
-	// trying something
-	// var now = new Date().getTime();
-	// var delta = now - stash.prevBeat;
-	
+	var id = stash.sampleCount % stash.peakBufferLength;
+	console.log("Updating index", id);
+	var circleClassName = "circle-" + id;
+	var avgClassName = "avgline";
+	var toleranceClassName = "toleranceline";
+
+	d3.select('.' + d3Stuff.className)
+		.selectAll('.' + circleClassName)
+		.data([soundImpulse])
+		.enter()
+		.append("circle")
+		.attr({
+			'class': circleClassName,
+			'style': 'stroke-width: 1; stroke: white',
+			'cx': function(impulse, i) { return d3Stuff.x(id)},
+			'cy': function(impulse, i) { return d3Stuff.y(impulse)},
+			'r': '2px'
+	});
+
+	d3.select('.' + d3Stuff.className)
+		.selectAll('.' + circleClassName)
+		.attr({
+			'cx': function(impulse, i) { return d3Stuff.x(id)},
+			'cy': function(impulse, i) { return d3Stuff.y(impulse)}
+	});
+
+	d3.select('.' + d3Stuff.className)
+	.selectAll('.' + avgClassName)
+	.data([soundImpulse])
+	.enter()
+	.append("line")
+	.attr({
+		'class': avgClassName,
+		'style': 'stroke-width: 1; stroke: green',
+		'x1': 0,
+		'x2': stash.width,
+		'y1': d3Stuff.y(stash.beatAverage),
+		'y2': d3Stuff.y(stash.beatAverage)
+	});
+
+	d3.select('.' + d3Stuff.className)
+		.selectAll('.' + avgClassName)
+		.attr({
+			y1: d3Stuff.y(soundAverage),
+			y2: d3Stuff.y(soundAverage)
+		});
+
+	d3.select('.' + d3Stuff.className)
+	.selectAll('.' + toleranceClassName)
+	.data([soundImpulse])
+	.enter()
+	.append("line")
+	.attr({
+		'class': toleranceClassName,
+		'style': 'stroke-width: 1; stroke: red',
+		'x1': 0,
+		'x2': stash.width,
+		'y1': d3Stuff.y(stash.peakTolerance * soundAverage),
+		'y2': d3Stuff.y(stash.peakTolerance * soundAverage)
+	});
+
+	d3.select('.' + d3Stuff.className)
+		.selectAll('.' + toleranceClassName)
+		.attr({
+			y1: d3Stuff.y(stash.peakTolerance * soundAverage),
+			y2: d3Stuff.y(stash.peakTolerance * soundAverage)
+		});
+
+	// stash.peakTolerance
 }
